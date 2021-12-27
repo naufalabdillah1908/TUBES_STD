807944b9360d56a1ef2747c97e31c7fced9a5490
naufalabdillah1908 <84857455+naufalabdillah1908@users.noreply.github.com>
2021-12-27 11:10:32 +0800

    create list
    create adrnya sama listnya

--- header.h
+++ header.h
@@ -11,26 +11,39 @@
 using namespace std;
 
 typedef struct elm_penyanyi *adr_penyanyi;
+typedef struct elm_lagu *adr_lagu;
 struct elm_penyanyi
 {
     string namaPenyanyi;
     string tempatTanggalLahir;
     adr_penyanyi next;
     adr_lagu nextLagu;
+    adr_lagu prevLagu;
 
 };
 
-typedef struct elm_lagu *adr_lagu;
+
 struct elm_lagu
 {
     string namaLagu;
     string artis;
     int tahunRilis;
     adr_lagu next;
+    adr_lagu prev;
 };
 
 struct musisi
 {
     adr_penyanyi first;
+    adr_penyanyi last;
 };
+
+struct lagu {
+    adr_lagu first;
+};
+
+adr_penyanyi create_musisi(string nama, string ttl);
+adr_lagu create_lagu(string nama, string artis, int tahun_rilis);
+void create_listMusisi(musisi &M);
+void create_listLagu(lagu &L);
 #endif // HEADER_H_INCLUDED
